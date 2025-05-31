8f54ce89df5eb17d39138f9038f5519590c94b0e
luizlhps <luizhenriquepinheirosilva@hotmail.com>
2025-05-31 19:36:14 -0300

    feat implement authentication email and gmail

---
+++ src/main/java/com/worklyze/worklyze/application/validations/NotZeroAndNotNullValidator.java
@@ -0,0 +1,19 @@
+package com.worklyze.worklyze.application.validations;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.math.BigDecimal;
+
+public class NotZeroAndNotNullValidator implements ConstraintValidator<NotZeroAndNotNullConstraint, BigDecimal> {
+
+    @Override
+    public void initialize(NotZeroAndNotNullConstraint constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(BigDecimal bigDecimal, ConstraintValidatorContext constraintValidatorContext) {
+        return bigDecimal != null && bigDecimal.compareTo(BigDecimal.ZERO) != 0;
+    }
+}
+
---
+++ src/main/java/com/worklyze/worklyze/infra/config/exception/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.worklyze.worklyze.infra.config.exception;
+
+import com.worklyze.worklyze.shared.exceptions.CustomException;
+import io.swagger.v3.oas.annotations.Hidden;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+
+@Hidden
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<RestErrorMessage> handleCustomException(CustomException ex) {
+        RestErrorMessage errorResponse = new RestErrorMessage(ex.getStatus(), ex.getMessage(), ex.getCode(), ex);
+        return new ResponseEntity<RestErrorMessage>(errorResponse, ex.getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<RestErrorMessage> handleCustomException(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors();
+        var fields = new HashMap<String, String>();
+
+        errors.forEach(e -> {
+            fields.put(e.getField(), e.getDefaultMessage());
+        });
+
+        RestErrorMessage errorResponse = new RestErrorMessage(HttpStatus.BAD_REQUEST, "Erro de validação", "BAD_REQUEST", ex, fields);
+
+        return new ResponseEntity<RestErrorMessage>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<RestErrorMessage> handleGeneralException(Exception ex) {
+        logger.error("Erro interno não tratado:", ex);
+
+        RestErrorMessage errorResponse = new RestErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, "Ocorreu um erro interno.", "INTERNAL_SERVER_ERROR", ex);
+
+        return new ResponseEntity<RestErrorMessage>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
