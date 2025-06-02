a437aa30772c728042295ae7345105327426d6c8
luizlhps <luizhenriquepinheirosilva@hotmail.com>
2025-06-01 22:50:09 -0300

    feat: implement surname

--- src/main/java/com/worklyze/worklyze/domain/entity/User.java
+++ src/main/java/com/worklyze/worklyze/domain/entity/User.java
@@ -23,6 +23,7 @@ public class User extends BaseEntity {
     private UUID id;
 
     private String name;
+    private String surname;
     private String username;
     private String email;
     private String password;
---
+++ src/main/resources/db/migration/V2__ADD_SURNAME.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "user"
+    ADD surname VARCHAR(255);
