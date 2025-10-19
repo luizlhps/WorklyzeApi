ee38f0f9a87c4c93075dce0c372c30e418c73911
luizlhps <luizhenriquepinheirosilva@hotmail.com>
2025-10-19 20:57:53 -0300

    refactor: typestatus name Capitalize

---
+++ src/main/resources/db/migration/V13__.sql
@@ -0,0 +1,4 @@
+update type_status set name = 'Aberto' where name = 'ABERTO';
+update type_status set name = 'Fechado' where name = 'FECHADO';
+update type_status set name = 'Desenvolvimento' where name = 'DENSEVOLVIMENTO';
+update type_status set name = 'Testes' where name = 'TESTES';
